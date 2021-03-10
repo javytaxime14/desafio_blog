b5afe4e84c151ba43095940deab82f3cbfd0672e
Javiera Hidalgo <javiera_56@hotmail.com>
2021-03-10 20:53:29 -0300

    last commit

--- config/puma.rb
+++ config/puma.rb
@@ -14,7 +14,7 @@ port        ENV.fetch("PORT") { 3000 }
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "production" }
-web: bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
+
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
